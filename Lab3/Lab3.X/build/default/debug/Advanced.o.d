232896c46da9be4066acb899fe87020ede704b54
IchBinTiMo <93331985+IchBinTiMo@users.noreply.github.com>
2021-11-04 11:26:16 +0800

    Delete Lab3 directory

--- Lab3/Lab3.X/build/default/debug/Advanced.o.d
+++
@@ -1,3 +0,0 @@
-build/default/debug/Advanced.o:  \
-	D:/Timo/MYSELF/NCKU/110-1/Microprocessor\ Principles\ and\ Applications/Lab/Lab3/Lab3.X/Advanced.asm  \
-
--- Lab3/Lab3.X/build/default/debug/Basic.o.d
+++
@@ -1,3 +0,0 @@
-build/default/debug/Basic.o:  \
-	D:/Timo/MYSELF/NCKU/110-1/Microprocessor\ Principles\ and\ Applications/Lab/Lab3/Lab3.X/Basic.asm  \
-
