436ae4761b7668e666cfda8472c27468b7bdd19c
IchBinTiMo <93331985+IchBinTiMo@users.noreply.github.com>
2021-11-04 11:36:56 +0800

    Add files via upload
    initial commit Lab 4

---
+++ 110_1/Microprocessor/Lab4/Lab4.X/Bonus.asm
@@ -0,0 +1,51 @@
+LIST p = 18f4520
+    #include<p18f4520.inc>
+	    CONFIG OSC = INTIO67
+	    CONFIG WDT = OFF
+	    org 0x00
+Initial:
+    SWPFF macro F1, F2
+	clrf TRISD
+	movff F2, TRISD
+	movff F1, F2
+	movff TRISD, F1
+	endm
+	
+    MOVLF macro literal, F
+	clrf WREG
+	movlw literal
+	movwf F
+	endm
+	
+    ADDFF macro F1, F2
+	clrf WREG
+	addwf F2, 0
+	addwf F1, 1
+	endm
+
+
+
+    
+Start:
+    clrf 000h
+    MOVLF 0x06, TRISC
+    MOVLF 0x00, TRISA
+    MOVLF 0x01, TRISB
+    rcall Fib_recur
+    rcall Finish
+    
+    
+    
+Fib_recur:
+    ADDFF TRISA, TRISB
+    SWPFF TRISA, TRISB
+    decfsz TRISC, 1
+    rcall Fib_recur
+    return
+    
+Finish:
+    nop
+    movff TRISA, 000h
+    end
+
+
---
+++ 110_1/Microprocessor/Lab4/Lab4.X/build/default/debug/Bonus.o.d
@@ -0,0 +1,3 @@
+build/default/debug/Bonus.o:  \
+	D:/Timo/MYSELF/NCKU/110-1/Microprocessor\ Principles\ and\ Applications/Lab/Lab4/Lab4.X/Bonus.asm  \
+
