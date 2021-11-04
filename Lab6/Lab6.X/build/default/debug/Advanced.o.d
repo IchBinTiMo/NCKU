a834c0361a646514762157130bb4dd821c2dfd42
IchBinTiMo <93331985+IchBinTiMo@users.noreply.github.com>
2021-11-04 11:17:36 +0800

    Add files via upload
    initial commit micorprocessor Lab6

---
+++ Lab6/Lab6.X/build/default/debug/Advanced.o.d
@@ -0,0 +1,3 @@
+build/default/debug/Advanced.o:  \
+	D:/Timo/MYSELF/NCKU/110-1/Microprocessor/Lab/Lab6/Lab6.X/Advanced.asm  \
+
---
+++ Lab6/Lab6.X/build/default/debug/Bonus.o.d
@@ -0,0 +1,3 @@
+build/default/debug/Bonus.o:  \
+	D:/Timo/MYSELF/NCKU/110-1/Microprocessor/Lab/Lab6/Lab6.X/Bonus.asm  \
+
